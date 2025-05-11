9045943302b832955857b266210a74a29957bd5c
Victor <victoralves06@gmail.com>
2025-05-11 16:27:27 -0300

    segundo commit

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ function verificar(){
 
     if(user.value.trim() == '' || senha.value.trim() == ''){
         erro.style.display = 'block';
-        setTimeout(() => {
-            erro.style.display = 'none';
-        }, 5000);
+        erro.textContent = 'Preencha todos os campos';
+        erro.style.color = 'red';
     }else{
         erro.textContent = 'Login Valido';
         erro.style.color = 'green';
         erro.style.display = 'block';
     }
+    
+    setTimeout(() => {
+            erro.style.display = 'none';
+        }, 5000);
 }
